add18fef85dacbc2fce21d41511e43881b8f4a84
Farid0558475484 <faridmh@code.edu.az>
2023-02-22 14:05:11 +0400

    feat:connect sql start

---
+++ FiorelloProject/DAL/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiorelloProject.DAL
+{
+	public class AppDbContext:DbContext
+	{
+		
+	}
+}
+
--- FiorelloProject/Program.cs
+++ FiorelloProject/Program.cs
@@ -1,7 +1,15 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using FiorelloProject.DAL;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer();
+
+});
 
 var app = builder.Build();
 
