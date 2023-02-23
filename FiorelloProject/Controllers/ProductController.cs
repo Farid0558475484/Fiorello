c330b90f5ac6cdec807b7f5ffacb43bd98e82c6f
Farid0558475484 <faridmh@code.edu.az>
2023-02-23 14:43:23 +0400

    feat:add product page

---
+++ FiorelloProject/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FiorelloProject.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiorelloProject.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly AppDbContext _appDbContext;
+
+        public ProductController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+
+            var products = _appDbContext.Products
+                .Include(p => p.ProductImages)
+                .Include(p => p.Category)
+                .ToList();
+
+            return View(products);
+        }
+    }
+}
