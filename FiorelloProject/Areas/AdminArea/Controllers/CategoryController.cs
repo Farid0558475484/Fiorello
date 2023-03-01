1f1b86358fca6c3f2614c76eefa8fec071661e81
Farid0558475484 <faridmh@code.edu.az>
2023-03-01 16:09:25 +0400

    feat:add create view and bag:category view

--- FiorelloProject/Areas/AdminArea/Controllers/CategoryController.cs
+++ FiorelloProject/Areas/AdminArea/Controllers/CategoryController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FiorelloProject.DAL;
+//using FiorelloProject.Migrations;
+using FiorelloProject.Models;
+
 
 namespace FiorelloProject.Areas.AdminArea.Controllers
 {
@@ -24,6 +27,33 @@ namespace FiorelloProject.Areas.AdminArea.Controllers
         {
             //return View();
             return View(_appDbContext.Categories.ToList());
+        }
+
+        public IActionResult Detail(int id)
+        {
+            if (id == null) return NotFound();
+            Category category = _appDbContext.Categories.SingleOrDefault(c => c.Id == id);
+            if (category == null) return NotFound();
+            return View(category);
+
+        }
+
+
+
+        public IActionResult Create()
+        {
+
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(string name)
+        {
+
+            return View($"{name}");
+
         }
     }
 }
