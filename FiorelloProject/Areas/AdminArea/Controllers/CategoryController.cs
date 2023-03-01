c18ecc1875c495f997ade738ce27dfa64600d90a
Farid0558475484 <faridmh@code.edu.az>
2023-03-01 14:42:11 +0400

    feat:add crud

--- FiorelloProject/Areas/AdminArea/Controllers/CategoryController.cs
+++ FiorelloProject/Areas/AdminArea/Controllers/CategoryController.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FiorelloProject.DAL;
 
 namespace FiorelloProject.Areas.AdminArea.Controllers
 {
+    
     [Area("AdminArea")]
+
+    
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
         public IActionResult Index()
         {
             //return View();
-            return Content("Category Dashboard");
+            return View(_appDbContext.Categories.ToList());
         }
     }
 }
