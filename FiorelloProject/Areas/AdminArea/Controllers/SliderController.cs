558be4eb103dbaa439181f47266bb4c547ad6337
Farid0558475484 <faridmh@code.edu.az>
2023-03-02 13:40:24 +0400

    feat:slidercreate

---
+++ FiorelloProject/Areas/AdminArea/Controllers/SliderController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FiorelloProject.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiorelloProject.Areas.AdminArea.Controllers
+{
+
+
+    [Area("AdminArea")]
+    public class SliderController : Controller
+
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public SliderController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(_appDbContext.Sliders.ToList());
+        }
+    }
+}
---
+++ FiorelloProject/ViewModels/SliderCreateVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FiorelloProject.ViewModels
+{
+	public class SliderCreateVM
+	{
+		public SliderCreateVM()
+		{
+		}
+	}
+}
+
