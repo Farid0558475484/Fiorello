84ed1f1c6e9f88a1ec025808b18576ef7baa54cc
Farid0558475484 <faridmh@code.edu.az>
2023-03-06 10:46:46 +0400

    feat:product controller add

---
+++ FiorelloProject/Areas/AdminArea/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FiorelloProject.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiorelloProject.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class ProductController : Controller
+    {
+
+
+        private readonly AppDbContext _appDbContext;
+
+        public ProductController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View(_appDbContext.Products
+                .Include(p=>p.ProductImages)
+                  .Include(p => p.Category)
+                  .ToList()
+                );
+        }
+    }
+}
