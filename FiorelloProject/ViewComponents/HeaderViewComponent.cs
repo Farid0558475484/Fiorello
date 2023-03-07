77e8dc213d5fc541c91c78b7d036d0b3012c31b9
Farid0558475484 <faridmh@code.edu.az>
2023-03-07 14:55:06 +0400

    feat:end register page and login logout home page

--- FiorelloProject/ViewComponents/HeaderViewComponent.cs
+++ FiorelloProject/ViewComponents/HeaderViewComponent.cs
@@ -5,23 +5,32 @@ using Microsoft.AspNetCore.Mvc;
 using FiorelloProject.Models;
 using FiorelloProject.ViewModels;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.Identity;
 
 namespace FiorelloProject.ViewComponents
 {
 	public class HeaderViewComponent:ViewComponent
 	{
 		private readonly AppDbContext _appDbContext;
+		private readonly UserManager<AppUser> _userManager;
 
-		public HeaderViewComponent(AppDbContext appDbContext)
+		public HeaderViewComponent(AppDbContext appDbContext, UserManager<AppUser> userManager)
 		{
 			_appDbContext = appDbContext;
-		}
+            _userManager = userManager;
+        }
 
-		public async Task<IViewComponentResult> InvokeAsync()
+  
+
+        public async Task<IViewComponentResult> InvokeAsync()
 		{
 
-
-
+			ViewBag.Fulname = string.Empty;
+			if(User.Identity.IsAuthenticated)
+			{
+				AppUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+				ViewBag.Fulname = user.Fulname;
+			}
 
             Bio bio = _appDbContext.Bios.FirstOrDefault();
 			return View(await Task.FromResult(bio));
